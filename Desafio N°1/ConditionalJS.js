65b65f3bc57bbcd596f160394c4b76089e509eca
makoret <fraco.1918@gmail.com>
2021-06-04 21:23:27 -0500

    desafio N°1 2-2

---
+++ Desafio N°1/ConditionalJS.js
@@ -0,0 +1,6 @@
+var Num = prompt("Escoge un numero:");
+if (Num > 10 && Num < 50) {
+    alert("Tu numero se encuentra entre 10 y 50!")
+} else {
+    alert("Tu numero NO se encuentra entre 10 y 50!")
+}
